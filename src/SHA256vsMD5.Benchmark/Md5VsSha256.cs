2d3915ad19b885c31f91987e5fdb1cb440ae523c
Tiago Santos <tfsantosbr@gmail.com>
2024-10-23 09:27:28 -0300

    upd

---
+++ src/SHA256vsMD5.Benchmark/Md5VsSha256.cs
@@ -0,0 +1,26 @@
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+
+namespace SHA256vsMD5.Benchmark;
+
+public class Md5VsSha256
+{
+    private const int N = 10000;
+    private readonly byte[] data;
+
+    private readonly SHA256 sha256 = SHA256.Create();
+    private readonly MD5 md5 = MD5.Create();
+
+    public Md5VsSha256()
+    {
+        data = new byte[N];
+        new Random(42).NextBytes(data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+
+}
---
+++ src/SHA256vsMD5.Benchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using SHA256vsMD5.Benchmark;
+
+var summary = BenchmarkRunner.Run<Md5VsSha256>();
